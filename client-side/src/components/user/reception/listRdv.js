d8e02067e1c6bd923835118858962fa8d861ed8c
Abderamane Oualili <a.oualili@esi-sba.dz>
2022-09-20 15:23:04 +0100

    fix export and validations buttons

--- client-side/src/components/user/reception/listRdv.js
+++ client-side/src/components/user/reception/listRdv.js
@@ -36,32 +36,57 @@ export default function ListRdv() {
 
   let tab = [...todos]
 
-        let namee = tab[0].name;
-        let directione = tab[0].direction;
-        let datee =  tab[0].date;
-        let heure_entreee = tab[0].heure_entree;
-        let titlee = tab[0].title;
-        let motife = tab[0].motif;
-        let type_rendezvouse = tab[0].type_rendezvous;
-
-  const addPdf = () => {
+  const addPdf = (self) => {
+    let da_rdv = tab.find(t => t.id == self.target.dataset.id)
+    console.log(da_rdv)
     Axios.post('http://localhost:3001/document.pdf', {
-        name: namee,
-        direction: directione,
-        date: datee,
-        heure_entree: heure_entreee,
-        title: titlee,
-        motif: motife,
-        type_rendezvous: type_rendezvouse,
+      // name: namee,
+      // direction: directione,
+      // date: datee,
+      // heure_entree: heure_entreee,
+      // title: titlee,
+      // motif: motife,
+      // type_rendezvous: type_rendezvouse,
+      rdv: da_rdv
     }).then((response) => {
-        console.log(response);
-        console.log(user)
-
-
-
+      console.log(response);
+      console.log(user)
     }).catch((err) => console.log(err));
-};
+  };
 
+  const valEnt = (self) => {
+    document.getElementById("cn-modal").style.display = "flex"
+    document.getElementById("cn-btn").dataset.rdv = self.target.dataset.ent
+  };
+
+  const valSor = (self) => {
+    document.getElementById("sortie-modal").style.display = "flex"
+    document.getElementById("sor-btn").dataset.rdv = self.target.dataset.sor
+  };
+
+  const entrerCN = (self) => {
+    let idrdv = self.target.dataset.rdv
+    console.log(idrdv)
+  };
+
+  const entrerHS = (self) => {
+    let idrdv = self.target.dataset.rdv
+    console.log(idrdv)
+  };
+
+
+  window.onclick = function (event) {
+    let cnmodal = document.getElementById("cn-modal")
+    let sortie_modal = document.getElementById("sortie-modal")
+    if (event.target == cnmodal) {
+      cnmodal.style.display = "none";
+      document.getElementById("cn-btn").dataset.rdv = -1
+
+    }
+    if (event.target == sortie_modal) {
+      sortie_modal.style.display = "none";
+    }
+  }
 
 
   let rdvs = {
@@ -73,79 +98,79 @@ export default function ListRdv() {
 
   return (
     <div className='font-poste mt-10'>
-    <div className="w-full max-w-4xl px-2 py-16 sm:px-0 mx-auto" id='modal_table'>
-      <Tab.Group>
-        <Tab.List className="flex space-x-1 rounded-xl bg-bleu p-1">
-          {Object.keys(rdvs).map((category) => (
-            <Tab
-              key={category}
-              className={({ selected }) =>
-                classNames(
-                  'w-full rounded-lg py-2.5 text-base leading-5 text-bleu',
-                  'ring-white ring-opacity-40 ring-offset-2 ring-offset-bleu focus:outline-none focus:ring-2',
-                  selected
-                    ? 'bg-white shadow'
-                    : 'text-jaune hover:bg-white/[0.12] '
-                )
-              }
-            >
-              RDVs {category}
-            </Tab>
-          ))}
-        </Tab.List>
-        <Tab.Panels className="mt-2">
-          {Object.values(rdvs).map((posts, idx) => (
-            <Tab.Panel
-              key={idx}
-              className={classNames(
-                'rounded-xl bg-white p-3',
-                'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
-              )}
-            >
-              {posts.map((post) => (
-                <div
-                  key={post.id}
-                  className="relative rounded-md mx-8 my-3 hover:bg-gray-100 grid grid-cols-3 grid-rows-3 gap-2"
-                >
-                  <div className="text-2xl place-self-start font-medium col-span-2 row-span-2 font-semibold my-auto">
-                    {post.nom} - {post.direction}
+      <div className="w-full max-w-4xl px-2 py-16 sm:px-0 mx-auto" id='modal_table'>
+        <Tab.Group>
+          <Tab.List className="flex space-x-1 rounded-xl bg-bleu p-1">
+            {Object.keys(rdvs).map((category) => (
+              <Tab
+                key={category}
+                className={({ selected }) =>
+                  classNames(
+                    'w-full rounded-lg py-2.5 text-base leading-5 text-bleu',
+                    'ring-white ring-opacity-40 ring-offset-2 ring-offset-bleu focus:outline-none focus:ring-2',
+                    selected
+                      ? 'bg-white shadow'
+                      : 'text-jaune hover:bg-white/[0.12] '
+                  )
+                }
+              >
+                RDVs {category}
+              </Tab>
+            ))}
+          </Tab.List>
+          <Tab.Panels className="mt-2">
+            {Object.values(rdvs).map((posts, idx) => (
+              <Tab.Panel
+                key={idx}
+                className={classNames(
+                  'rounded-xl bg-white p-3',
+                  'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
+                )}
+              >
+                {posts.map((post) => (
+                  <div
+                    key={post.id}
+                    className="relative rounded-md mx-8 my-3 hover:bg-gray-100 grid grid-cols-3 grid-rows-3 gap-2"
+                  >
+                    <div className="text-2xl place-self-start font-medium col-span-2 row-span-2 font-semibold my-auto">
+                      {post.nom} - {post.direction}
+                    </div>
+
+                    <div className={classNames('row-span-2 my-auto font-semibold place-self-center',
+                      post.heure_sortie ? 'row-span-1' : 'row-span-2')}>Heure d'entree: {post.heure_entree} </div>
+
+                    {post.heure_sortie ? <div className='row-span-1 my-auto font-semibold place-self-center'>Heure de sortie: {post.heure_sortie} </div> : ''}
+
+
+                    <div className='col-span-1 place-self-start'>Date: {post.date}</div>
+
+                    {post.heure_sortie ?
+                      '' :
+                      post.validation === 0 ?
+                        <div className='col-span-2 place-self-end'>
+                          <button onClick={valEnt} data-ent={post.id} className='bg-jaune text-bleu rounded-md px-8 py-2 '>
+                            Valider l'entree
+                          </button>
+                        </div>
+                        :
+                        < div className='col-span-2 place-self-end	'>
+                          <button onClick={valSor} data-sor={post.id} className='bg-jaune text-bleu rounded-md px-8 py-2 mx-2 '>
+                            Valider la sortie
+                          </button>
+                          <button onClick={addPdf} data-id={post.id} className='bg-bleu text-white rounded-md px-8 py-2 mx-2'>
+                            Export
+                          </button>
+                        </div>
+
+                    }
+
+
                   </div>
-
-                  <div className={classNames('row-span-2 my-auto font-semibold place-self-center',
-                    post.heure_sortie ? 'row-span-1' : 'row-span-2')}>Heure d'entree: {post.heure_entree} </div>
-
-                  {post.heure_sortie ? <div className='row-span-1 my-auto font-semibold place-self-center'>Heure de sortie: {post.heure_sortie} </div> : ''}
-
-
-                  <div className='col-span-1 place-self-start'>Date: {post.date}</div>
-
-                  {post.heure_sortie ?
-                    '' :
-                    post.validation === 0 ?
-                      <div className='col-span-2 place-self-end'>
-                        <button className='bg-jaune text-bleu rounded-md px-8 py-2 '>
-                          Valider l'entree
-                        </button>
-                      </div>
-                      :
-                      < div className='col-span-2 place-self-end	'>
-                        <button className='bg-jaune text-bleu rounded-md px-8 py-2 mx-2 '>
-                          Valider la sortie
-                        </button>
-                        <button onClick={addPdf} className='bg-bleu text-white rounded-md px-8 py-2 mx-2'>
-                          Export
-                        </button>
-                      </div>
-
-                  }
-
-        
-                </div>
-              ))}
-            </Tab.Panel>
-          ))}
-        </Tab.Panels>
-      </Tab.Group>
+                ))}
+              </Tab.Panel>
+            ))}
+          </Tab.Panels>
+        </Tab.Group>
       </div >
 
       <div className="fixed hidden inset-0 bg-gray-600 bg-opacity-50  overflow-y-auto h-full items-center justify-center backdrop-blur-sm" id="cn-modal">
@@ -163,29 +188,29 @@ export default function ListRdv() {
               autoComplete="off"
               className="mt-1 block w-full rounded-md border border-gray-300 h-6 p-4"
             />
-            <button className='bg-bleu text-white rounded-md px-8 py-2 m-2 place-self-end'>
+            <button className='bg-bleu text-white rounded-md px-8 py-2 m-2 place-self-end' id="cn-btn" onClick={entrerCN}>
               Continuer
             </button>
           </div>
         </div>
       </div>
 
-      <div className="fixed hidden inset-0 bg-gray-600 bg-opacity-50  overflow-y-auto h-full items-center justify-center backdrop-blur-sm" id="cn-modal">
+      <div className="fixed hidden inset-0 bg-gray-600 bg-opacity-50  overflow-y-auto h-full items-center justify-center backdrop-blur-sm" id="sortie-modal">
         <div className="relative p-4 lg:w-2/5 w-3/4 shadow-lg rounded-md bg-gris text-xl" >
           <div className='mt-5 sm:mt-0 px-8 py-2 '>
             <label htmlFor="name" className="block font-bold text-gray-700 p-4 mb-4 sm:px-0 bg-jaune rounded-full mx-24 text-bleu">
               Entrer l'heure de sortie
             </label>
             <input
-              placeholder='numero de la piece d identitee'
+              placeholder='Heure de sortie'
               type="time"
-              name="cn"
-              id="cn"
+              name="hs"
+              id="hs"
               required
               autoComplete="off"
               className="mt-1 block w-full rounded-md border border-gray-300 p-4"
             />
-            <button className='bg-bleu text-white rounded-md px-8 py-2 m-2 place-self-end'>
+            <button className='bg-bleu text-white rounded-md px-8 py-2 m-2 place-self-end' id='sor-btn' onClick={entrerHS}>
               Continuer
             </button>
           </div>
